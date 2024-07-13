06c8f29ddcd0652a2ba63751827941fb9167b4fe
Serge Voloshenko <serge.voloshenko@mhs.com>
2024-07-13 01:06:18 -0400

    WebApp demo VS app

--- azure-cli.ps1
+++ azure-cli.ps1
@@ -3,11 +3,15 @@ az login
 az account show --output table
 az account set --subscription "Visual Studio Enterprise Subscription"
 
-# Create a service principal and paste the IDs into ubuntu.json file 
-az ad sp create-for-rbac --role Contributor --scopes /subscriptions/f7806be4-19fa-45cf-b6e7-636c8ebd49d9 --query "{ client_id: appId, client_secret: password, tenant_id: tenant }"
-az role assignment list --scope /subscriptions/"f7806be4-19fa-45cf-b6e7-636c8ebd49d9" --role "Contributor"
-az role assignment list --assignee "3dbd575d-64a2-4775-9a2c-faedd0ca84fe" # --assignee = "{client_id}}""
-
 # Obtain your Azure subscription ID
 az account show --query "{ subscription_id: id }"
+
+# Create a service principal and with contributor access to the full subscribtion 
+az ad sp create-for-rbac --name gitRepo-workflow-for-azure --role Contributor --scopes /subscriptions/f7806be4-19fa-45cf-b6e7-636c8ebd49d9 --query "{ client_id: appId, tenant_id: tenant }"
+
+# Grant Federated Credentals to Github Repo: go to Azure Portal > MS Entra ID > Manage > App Registration > Owened Applications > Select created Service Provider > Manage > Certificates & Secrets > Federated Credentals > Add new Federated Credentials pemission.
+# Go to your GitHub repo settings > Secrets & Variables > Actions >  and create new variables
+# AZURE_CLIENT_ID
+# AZURE_SUBSCRIPTION_ID
+# AZURE_TENANT_ID
 
